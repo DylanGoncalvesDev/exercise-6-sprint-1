5bd2cffa36c74d30915e4c4af35e1728a8b0fcf9
Dylan Goncalves <dylanphx93@gmail.com>
2026-04-26 17:42:48 +0200

    fix mistakes

--- level-1/index.php
+++ level-1/index.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-if (!empty($_POST["usuario"]) && !empty($_POST["email"]) && !empty($_POST["contraseña"])) {
+if (!empty($_POST["usuario"]) && !empty($_POST["email"]) && !empty($_POST["contrasena"])) {
     $usuario = $_POST["usuario"];
     $email = $_POST["email"];
-    $contraseña = ["contraseña"];
+    $contraseña = $_POST["contrasena"];
     
     $_SESSION["usuario"] = $usuario; 
 
