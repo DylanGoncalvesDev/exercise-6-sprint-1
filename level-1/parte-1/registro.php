29cdb48505cbdf3bd3194745b14e684bbdfe6f95
Dylan Goncalves <dylanphx93@gmail.com>
2026-04-27 00:15:36 +0200

    divided the level one  in parts and fixen mistakes in the registro.php file

--- level-1/parte-1/registro.php
+++ level-1/parte-1/registro.php
@@ -3,19 +3,21 @@
 session_start();
 
  function procesarFormulario(): string {
+  $mensaje = "";
   if (!empty($_POST["usuario"]) && !empty($_POST["email"]) && !empty($_POST["contrasena"])) {
       $usuario = $_POST["usuario"];
       $email = $_POST["email"];
-      $contraseña = $_POST["contrasena"];
+      $contrasena = $_POST["contrasena"];
       $_SESSION["usuario"] = $usuario; 
-   }
 
-   return "Sus Datos Usuario: " . $usuario . "Email: " . $email . "y Contraseña: " . $contraseña 
-           . "se han registrado correctamente";
+      $mensaje = "Sus Datos / Usuario: " . $usuario . " / Email: " . $email . " / Contraseña: " . $contrasena 
+      . " Se han Registrado Correctamente";
+  } else {
+      $mensaje = "ERROR: Datos Inexistentes";
+  }
+   return $mensaje;
 }
 
 echo procesarFormulario ();
-
-
 
 ?> 
