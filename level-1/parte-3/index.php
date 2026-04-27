d224d62e735eef71db350be54c69ec9b86d742c5
Dylan Goncalves <dylanphx93@gmail.com>
2026-04-27 13:54:44 +0200

    complete the part three

--- level-1/parte-3/index.php
+++ level-1/parte-3/index.php
@@ -1,7 +1,24 @@
 <?php
 //Parte 3//
+require "Calculable.php";
+require "Shape.php";
+require "Circulo.php";
+require "Rectangulo.php";
+require "Triangulo.php";
 
+$circulo1 = new Circulo (23);
+$rectangulo1 = new Rectangulo (12 , 5);
+$triangulo1 = new Triangulo (17 , 33);
 
+//llamamos al metodo magico invoke//
+echo $circulo1() . "\n";
+echo $triangulo1() . "\n";
+echo $rectangulo1() . "\n";
+
+//llamamos al metodo magico toString//
+echo $circulo1 . "\n";
+echo $rectangulo1 . "\n";
+echo $triangulo1 . "\n";
 
 ?> 
 
